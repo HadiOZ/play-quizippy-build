fdb61b8d6830ea23f2fd2d443dfaff74fdc2d798
Iqbal Hadi Purnomo <hadioz616@gmail.com>
2021-02-19 20:45:34 +0700

    this is first commit

---
+++ precache-manifest.9d9d79b853c296efff980af05cecc3e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3030f8f4d213ac3543e3f520fc8bcf32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2018a70aaf12fbce410",
+    "url": "/static/css/main.190c8067.chunk.css"
+  },
+  {
+    "revision": "2775d5a836e871166a63",
+    "url": "/static/js/2.a431840c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a431840c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2018a70aaf12fbce410",
+    "url": "/static/js/main.90242c86.chunk.js"
+  },
+  {
+    "revision": "af34fa8a3119670151a7",
+    "url": "/static/js/runtime-main.93b41a56.js"
+  },
+  {
+    "revision": "c271116b531921ffc707e82ec1d0769b",
+    "url": "/static/media/logoquizippy.c271116b.png"
+  }
+]);
